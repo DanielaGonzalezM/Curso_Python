b4c019215867c12510a402670148f48c0ea18033
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-11-10 15:17:15 -0300

    Sección 26: Manejo de Transacciones con Python y Postgresql

--- 12-db/eliminar_varios.py
+++ 12-db/eliminar_varios.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-conexion = psycopg2.connect(user='postgres', password='admin',
+conexion = psycopg2.connect(user='postgres1', password='admin',
                             host='127.0.0.1', port='5432', database='test_db')
 
 try:
